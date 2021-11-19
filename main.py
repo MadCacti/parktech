24a3d7eeda4c82170c81b5de5980a13fc5834603
Raiden <raiden12121@gmail.com>
2021-11-19 13:43:40 -0800

    add api, fix background

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
-# from newsapi.newsapi_client import NewsApiClient
+from newsapi.newsapi_client import NewsApiClient
 import requests
 # create a Flask instance
 app = Flask(__name__)
+
+yourAPIKEY = '8169dc4f99474483ab5999bc2c761381'  # write your API key here
+newsapi = NewsApiClient(api_key=yourAPIKEY)
 
 
 # connects default URL to render index.html
@@ -14,8 +17,20 @@ def index():
 
 @app.route('/raiden/')
 def raiden():
-    return render_template("profiles/raiden.html")
+    return render_template("profiles/raiden.html", news='')
 
+
+@app.route('/raiden/results/', methods=['POST'])
+def get_results():
+    keyword = request.form['keyword']  # getting input from user
+
+    news = newsapi.get_top_headlines(q=keyword,
+                                     # sources='bbc-news,the-verge',#optional and you can change
+                                     # category='business', #optional and you can change also
+                                     language='en',  # optional and you can change also
+                                     country='in')
+    # print(news['articles'])
+    return render_template('profiles/raiden.html', news=news['articles'])
 
 @app.route('/paul/')
 def paul():
